155a8a58923e22ccd9cdaa4dda409e0e3c845079
Dave Parfitt <metadave@protonmail.com>
2019-12-22 07:50:14 -0500

    wip

--- runtime/Rust/antlr4-rust/src/interval.rs
+++ runtime/Rust/antlr4-rust/src/interval.rs
@@ -4,7 +4,9 @@ use std::cmp::max;
 
 pub use crate::token::TokenType;
 
-#[derive(Debug, PartialEq)]
+
+/** An immutable inclusive interval a..b */
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Interval {
     pub a: i32,
     pub b: i32, // stop is not included
@@ -16,14 +18,7 @@ impl Interval {
     }
 
     pub fn contains(&self, item: i32) -> bool {
-        item >= self.a && item < self.b
-    }
-
-    pub fn string(&self) -> String {
-        if self.a == self.b - 1 {
-            return self.a.to_string();
-        }
-        return self.to_string();
+        item >= self.a && item <= self.b
     }
 
     pub fn length(&self) -> i32 {
@@ -104,11 +99,7 @@ impl Interval {
 
 impl fmt::Display for Interval {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.a == self.b - 1 {
-            write!(f, "{}", self.a)
-        } else {
-            write!(f, "{}..{}", self.a, self.b - 1)
-        }
+        write!(f, "{}..{}", self.a, self.b)
     }
 }
 
@@ -119,11 +110,37 @@ pub struct IntervalSet {
     read_only: bool,
 }
 
+
 impl IntervalSet {
     pub fn new() -> IntervalSet {
         IntervalSet {
             intervals: Vec::new(),
             read_only: false,
+        }
+    }
+
+    pub fn new_from_intervals(ivs:Vec<Interval>) -> IntervalSet {
+        IntervalSet {
+            intervals: ivs,
+            read_only: false,
+        }
+    }
+
+    pub fn of(&mut self, a:i32, b:i32) {
+        self.intervals.push(Interval::new(a, b));
+    }
+
+    // TODO: better name?
+    pub fn of_same(&mut self, a:i32) {
+        self.intervals.push(Interval::new(a, a));
+    }
+
+    pub fn clear(&mut self) -> Result<(), &str> {
+        if self.read_only {
+            return Err("can't alter readonly IntervalSet")
+        } else {
+            self.intervals.clear();
+            return Ok(())
         }
     }
 
@@ -134,12 +151,47 @@ impl IntervalSet {
         return Ok(self.intervals[0].a);
     }
 
-    fn add(&self, addition: Interval) -> Result<(), &str> {
+    fn add(&mut self, addition: Interval) -> Result<(), &str> {
         if self.read_only {
             return Err("Can't alter readonly IntervalSet");
         } else {
-            for r in &self.intervals {
-                println!("{:?}", r);
+            for index in 0..self.intervals.len() {
+                let r = self.intervals[index];
+                if addition == r {
+                    return Ok(())
+                }
+                
+                if addition.adjacent(&r) || ! addition.disjoint(&r) {
+				    // next to each other, make a single larger interval
+                    let bigger:Interval = addition.union(&r);
+                    self.intervals[index] = bigger;
+                    
+                    // make sure we didn't just create an interval that
+				    // should be merged with next interval in list
+                    println!("index = {}", index);
+                    println!("len = {}", self.intervals.len());
+                    
+                    let mut i = index;
+                    while i < self.intervals.len() - 1 {
+                        i += 1;
+                        let next = self.intervals[i];
+                        if !bigger.adjacent(&next) && bigger.disjoint(&next) {
+                            break
+                        }
+                        let even_bigger = bigger.union(&next);
+                        self.intervals.remove(i);
+                        i -=1;
+                        self.intervals[i] = even_bigger;
+
+                    }
+                    return Ok(());
+                }
+                if addition.starts_before_disjoint(&r) {
+                    // insert before r
+                    self.intervals.insert(index, addition);
+                    println!("G");
+                    return Ok(());
+                }
             }
             return Ok(());
         }
@@ -151,4 +203,92 @@ impl IntervalSet {
     // func (i *IntervalSet) addOne(v int) {
     //     i.addInterval(NewInterval(v, v+1))
     // }
+}
+
+
+impl fmt::Display for IntervalSet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {        
+        let l = self.intervals.iter()
+                    .map(|&x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ");
+        write!(f, "[{}]", l)
+    }
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interval() {
+        let x = Interval::new(1, 5);
+        assert_eq!("1..5".to_string(), x.to_string());
+
+        for i in 1..5 {
+            assert!(x.contains(i));
+        }
+        assert!(!x.contains(6));
+    }
+
+    #[test]
+    fn test_set_stuff() {
+        let x = Interval::new(1, 5);
+        let y = Interval::new(6, 10);
+        let z = Interval::new(2, 6);
+    
+        assert!(x.starts_before_disjoint(&y));
+        assert!(x.starts_before_non_disjoint(&z));
+        assert!(!y.starts_before_non_disjoint(&z));
+    }
+    
+    #[test]
+    fn test_interval_set_merge() {
+        let vs = vec![Interval::new(1,4), Interval::new(7,8)];
+        let mut iset = IntervalSet::new_from_intervals(vs);
+        let z = Interval::new(2, 6);
+        assert!(iset.add(z).is_ok());
+        assert_eq!(iset.intervals.len(), 1);
+        assert_eq!(iset.intervals[0], Interval::new(1,8));
+    }
+
+    #[test]
+    fn test_interval_set_extended_merge() {
+        let vs = vec![Interval::new(1,4), Interval::new(7,8), Interval::new(10,12)];
+        let mut iset = IntervalSet::new_from_intervals(vs);
+        println!("-----> {}", iset);
+        let z = Interval::new(2, 13);
+        assert!(iset.add(z).is_ok());
+        println!("-----> {}", iset);
+        
+        //assert_eq!(iset.intervals.len(), 1);
+        //assert_eq!(iset.intervals[0], Interval::new(1,8));
+    }
+
+    #[test]
+    fn test_interval_set_middle() {
+        let vs = vec![Interval::new(1,4), Interval::new(10,12)];
+        let mut iset = IntervalSet::new_from_intervals(vs);
+        let z = Interval::new(6, 8);
+        assert!(iset.add(z).is_ok());
+        assert_eq!(iset.intervals.len(), 3);
+        assert_eq!(iset.intervals[0], Interval::new(1,4));
+        assert_eq!(iset.intervals[1], Interval::new(6,8));
+        assert_eq!(iset.intervals[2], Interval::new(10,12));
+    }
+
+    #[test]
+    fn test_interval_set_first() {
+        let vs = vec![Interval::new(4,5), Interval::new(10,12)];
+        let mut iset = IntervalSet::new_from_intervals(vs);
+        let z = Interval::new(1, 2);
+        assert!(iset.add(z).is_ok());
+        assert_eq!(iset.intervals.len(), 3);
+        assert_eq!(iset.intervals[0], Interval::new(1,2));
+        assert_eq!(iset.intervals[1], Interval::new(4,5));
+        assert_eq!(iset.intervals[2], Interval::new(10,12));
+    }
 }
--- runtime/Rust/antlr4-rust/src/lib.rs
+++ runtime/Rust/antlr4-rust/src/lib.rs
@@ -1,3 +1,8 @@
+pub mod interval;
+pub mod int_stream;
+pub mod char_stream;
+pub mod token;
+
 
 #[cfg(test)]
 mod tests {
@@ -5,10 +10,10 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn test_interval() {
+        let x = crate::interval::Interval::new(1, 5).to_string();
+        assert_eq!("1..5".to_string(), x.to_string());
+    }
 }
-
-
-pub mod interval;
-pub mod int_stream;
-pub mod char_stream;
-pub mod token;
